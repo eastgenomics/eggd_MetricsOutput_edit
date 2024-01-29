2fd44c65592e55a47eb5e974fc6330f833b3589e
Arun <arun.karnani.k@gmail.com>
2024-01-29 16:56:05 +0000

    Fixing minor PEP8 warnings 	modified:   process_metrics_file.py

--- process_metrics_file.py
+++ process_metrics_file.py
@@ -27,6 +27,7 @@ from openpyxl.formatting.rule import CellIsRule
 RED_TEXT = Font(name='Calibri', color="9C0006")
 RED_FILL = PatternFill("solid", fgColor="FFC7CE")
 
+
 class Excel():
     """
     Functions for formatting excel data with the appropriate style and
@@ -50,7 +51,8 @@ class Excel():
 
     def modify(self) -> None:
         """
-        Calls all methods in excel() to modify and generate output file with desired changes
+        Calls all methods in excel() to modify and generate output file
+        with desired changes
         """
         self.move_rows()
         self.mark_false()
@@ -61,13 +63,14 @@ class Excel():
 
     def move_rows(self) -> None:
         """
-        Excel method which modifies its input by moving the set of rows 
-        from 16 to 19 by 2 columns towards the right. 
+        Excel method which modifies its input by moving the set of rows
+        from 16 to 19 by 2 columns towards the right.
 
-        This is to shift the [Analysis status] table to allign sample metrics with the rest
-        of the tables from the excel file.
+        This is to shift the [Analysis status] table to allign sample metrics
+        with the rest of the tables from the excel file.
         """
-        # Obtain the last column letter of the excel file and store it a variable
+        # Obtain the last column letter of the excel file and store it
+        # in a variable
         max_column = get_column_letter(self.ws.max_column - 2)
 
         self.ws.move_range(f'B16:{max_column}19', cols=2)
@@ -78,8 +81,10 @@ class Excel():
         Mark in red all cells with string FALSE
         """
         string_to_find = "FALSE"
-        # store a list of cell indices where the contain a string equal to "FALSE"
-        false_cells_indices = self.df.stack().index[self.df.stack() == string_to_find]
+        # store a list of cell indices where the contain a string equal to
+        # "FALSE"
+        false_cells_indices = self.df.stack().index[self.df.stack() == 
+                                                    string_to_find]
 
         # Mark every cell from the list in red
         for idx in false_cells_indices:
@@ -109,21 +114,21 @@ class Excel():
 
                     value_to_compare = self.df.loc[row][sample_col_index]
 
-                    # Ensure that each string from the elements_to_find are found in the 
-                    # first column
+                    # Ensure that each string from the elements_to_find are
+                    # found in the first column
                     if first_column == 0:
                         LSL = self.df.loc[row][LSL_column_index]
                         USL = self.df.loc[row][USL_column_index]
 
-                # Store a boolean for each sample and set to false when any metric does
-                # not exceed the USL and LSL thresholds
+                # Store a boolean for each sample and set to false when any
+                # metric does not exceed the USL and LSL thresholds
                 if value_to_compare < LSL or value_to_compare > USL:
                     sample_to_highlight = True
                 else:
                     sample_to_highlight = False
                     break
 
-            # If boolean remains true, highlight every value from that sample column
+            # If boolean remains true, highlight every value for sample
             if sample_to_highlight is True:
                 for element in elements_to_find:
                     indices = self.df.stack().index[self.df.stack() == element]
@@ -153,8 +158,8 @@ class Excel():
                            'PCT_ON_TARGET_READS (NA)',
                            'SCALED_MEDIAN_GENE_COVERAGE (NA)',
                            'TOTAL_PF_READS (NA)']
-        
-        # Search for the cell location for every metric from metrics_to_find list
+
+        # Search for the cell location for every metric from metrics list
         for metric in metrics_to_find:
             indices = self.df.stack().index[self.df.stack() == metric]
 
@@ -163,9 +168,9 @@ class Excel():
                 row = idx[0]
                 LSL = self.df.loc[row][idx[1]+1]
                 USL = self.df.loc[row][idx[1]+2]
-            
-            # Select the appropriate operator and formula based on the the values of LSL
-            # and USL
+
+            # Select the appropriate operator and formula based on
+            # the values of LSL and USL
             if LSL == 'NA' and USL == 'NA':
                 pass
             elif LSL == 'NA':
@@ -178,10 +183,11 @@ class Excel():
                 operator = 'notBetween'
                 formula = [LSL, USL]
             
-            # for every row, apply selected operator and formula to highlight cells 
+            # for every row, apply selected operator and formula
+            # to highlight cells
             rule = CellIsRule(operator=operator, formula=formula,
-                                stopIfTrue=False, fill=RED_FILL,
-                                font=RED_TEXT)
+                              stopIfTrue=False, fill=RED_FILL,
+                              font=RED_TEXT)
             self.ws.conditional_formatting.add(f'D{row+1}:{max_column}{row+1}',
                                                rule)
 
@@ -194,12 +200,15 @@ def parse_args() -> argparse.Namespace:
         args : Namespace
           Namespace of passed command line argument inputs
     """
-    parser = argparse.ArgumentParser(description='MetricsOutput.tsv converter and editor')
+    parser = argparse.ArgumentParser(description='MetricsOutput.tsv \
+                                     converter and editor')
 
     parser.add_argument('tsv_input', type=str,
                         help='filepath to MetricsOutput.tsv file')
-    parser.add_argument('-o', '--output_filename', type=str, default='MetricsOutput.xlsx',
-                        help='OPTIONAL: Output filename, default set to MetricsOutput.xlsx')
+    parser.add_argument('-o', '--output_filename', type=str,
+                        default='MetricsOutput.xlsx',
+                        help='OPTIONAL: Output filename, default set to \
+                              MetricsOutput.xlsx')
     args = parser.parse_args()
 
     return args
@@ -219,7 +228,7 @@ def tsv_to_excel(input_filepath, output_filepath):
 
     # Convert .tsv file to excel
     with open(input_filepath, 'r', encoding='UTF-8') as csvfile:
-        read_tsv = csv.reader(csvfile, delimiter = '\t')
+        read_tsv = csv.reader(csvfile, delimiter='\t')
         for row in read_tsv:
             converted_row = []
             for item in row:
@@ -239,7 +248,8 @@ def tsv_to_excel(input_filepath, output_filepath):
 
 def main():
     """
-    Main entry points to run the script. Creates an .xlsx file when run correctly
+    Main entry points to run the script. Creates an .xlsx file when run
+    correctly
     """
     args = parse_args()
 
@@ -250,6 +260,7 @@ def main():
 
     print(args.tsv_input)
     print(args.output_filename)
+
 
 if __name__ == '__main__':
     main()
